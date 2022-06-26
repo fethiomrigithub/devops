bbcc5933c2a99539c837acd3863ac2a4fd53c3d1
fethiomrigithub <108096872+fethiomrigithub@users.noreply.github.com>
2022-06-26 22:26:08 +0200

    Update ShapeTest.java

--- ShapeTest.java
+++ ShapeTest.java
@@ -6,9 +6,9 @@ public class ShapeTest {
  @Test
  public void shouldCalculateRadius() {
  System.out.println("running test 1: addition");
- Assertions.assertEquals(8, c.getRadius()),  "Expected  radius result did not match returned value");
- Assertions.assertEquals(7, c.getRadius()),  "Expected  radius result  match returned value");
-   Assertions.assertEquals(15, c.getDiameter()),  "Expected  radius result did not match returned value");
+ Assertions.assertEquals(8, c.getRadius(),  "Expected  radius result did not match returned value");
+ Assertions.assertEquals(7, c.getRadius(),  "Expected  radius result  match returned value");
+   Assertions.assertEquals(15, c.getDiameter(),  "Expected  radius result did not match returned value");
    
  }
 
